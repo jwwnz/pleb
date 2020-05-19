ff8f304a7947cb42502c49b395d628cadfb74635
jwwnz <jwon117@aucklanduni.ac.nz>
2020-05-19 22:27:30 +1200

    Add some content into the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,28 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
+
+const legalLinks = [
+	{
+		name: "Law Society complaints",
+		link:
+			"https://www.lawsociety.org.nz/for-the-community/lawyers-complaints-service",
+		description:
+			"This is the detailed official website which outlines how to make a complaint about a lawyer",
+	},
+	{
+		name: "Contractual obligations",
+		link: "https://www.consumer.org.nz/articles/contract-law",
+		description:
+			"This is a great summary of obligations when you sign up to a contract.",
+	},
+];
 
 function HomePage() {
 	const homePageStyle = {
-		padding: "100px",
+		padding: "100px 0px 0px 0px",
+	} as React.CSSProperties;
+
+	const quoteStyle = {
 		fontStyle: "italic",
 	} as React.CSSProperties;
 
@@ -11,7 +30,25 @@ function HomePage() {
 		<>
 			<Container style={homePageStyle}>
 				<Row>
-					<Col>Use the law. It's yours as much as anyones.</Col>
+					<Col>
+						<Image src="people.jpg" fluid />
+					</Col>
+				</Row>
+				<Row>
+					<Col style={quoteStyle}>
+						Use the law. It's yours as much as anyones.
+					</Col>
+				</Row>
+				<hr style={{ width: "100px" }} />
+				<Row>
+					<Col>
+						<h4>Important links</h4>
+						{legalLinks.map(({ name, link, description }) => (
+							<div>
+								<a href={link}>{name}</a> - <span>{description}</span>
+							</div>
+						))}
+					</Col>
 				</Row>
 			</Container>
 		</>
