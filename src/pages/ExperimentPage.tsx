a976a673e8f6467e5f807a4855bbaeec5f465963
jwwnz <jwon117@aucklanduni.ac.nz>
2020-07-12 18:14:38 +1200

    Adding React-select and implement simple select component.

--- src/pages/ExperimentPage.tsx
+++ src/pages/ExperimentPage.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import Select from "react-select";
 import "./HomePage.css";
 import { legalLinks } from "../test-data/test-data";
+
+const options = [{ value: "conveyancing", label: "Conveyancing (Property)" }];
 
 function ExperimentPage() {
 	return (
 		<>
 			<Container>
-				<div>Experiment</div>
+				<div>What legal area do you need information?</div>
+				<Select options={options} />
 			</Container>
 		</>
 	);
